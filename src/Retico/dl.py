0366fab77a7886819161a768fb80bc7aa809ad47
Sebastian Wilharm <swilharm@uni-potsdam.de>
2022-09-09 20:55:45 +0200

    Refactored Deep Learning Code

--- src/Retico/dl.py
+++ src/Retico/dl.py
@@ -10,9 +10,23 @@ import sklearn
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
-# Load the dataset
-X_df = pd.read_json('data/X_DM.json', orient = 'index')
-y_df = pd.read_json('data/y_DM.json', orient = 'index')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Model for task 1
+class Net(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Net, self).__init__()
+
+        self.hidden0 = nn.Linear(input_size, hidden_size)
+        self.hidden1 = nn.Linear(hidden_size, hidden_size)
+        self.output = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.hidden0(x))
+        x = F.relu(self.hidden1(x))
+        x = self.output(x)
+        return x
+
 
 def convert_datatype(X_df, y_df):
     ### convert the datatype from list object to float ####
@@ -38,168 +52,157 @@ def convert_datatype(X_df, y_df):
 
     # Tensorize X and y
     array_X = final_df_noisy.values
-    tensor_X = torch.FloatTensor(array_X) 
+    tensor_X = torch.FloatTensor(array_X)
     tensor_y = torch.LongTensor(y_df.values).view(-1)
 
     return tensor_X, tensor_y
-    
-tensor_X, tensor_y = convert_datatype(X_df, y_df)
-print (tensor_X.shape, tensor_y.shape)
 
-# Data split
-X_train, X_test, y_train, y_test = train_test_split(tensor_X, tensor_y, test_size=1/3, random_state=42)
 
-# Task 1: Predict coordinate (y-hat)
-# Hyperparameter
-input_size = X_train.shape[1]
-hidden_size = 32
-output_size = 15 + 2 # p + No instruction given + Language
-lr = 0.01
-batch_size = 512
-n_epochs = 10
-report_every = 1
+def pred_coord(X, y):
+    """Task 1: Predict coordinate (y-hat)"""
 
-# Model for task 1
-class Net(nn.Module):
+    # Data split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3, random_state=42)
 
-    def __init__(self, input_size, hidden_size, output_size):
-        super(Net, self).__init__()
-        
-        self.hidden0 = nn.Linear(input_size, hidden_size)
-        self.hidden1 = nn.Linear(hidden_size, hidden_size)
-        self.output = nn.Linear(hidden_size, output_size)
+    # Hyperparameter
+    input_size = X_train.shape[1]
+    hidden_size = 32
+    output_size = 15 + 2  # p + No instruction given + Language
+    lr = 0.01
+    batch_size = 512
+    n_epochs = 30
+    report_every = 3
 
-    def forward(self, x):
-        x = F.relu(self.hidden0(x))
-        x = F.relu(self.hidden1(x))
-        x = self.output(x)
-        return x
-
-def pred_coord(X_train, X_test, y_train, y_test):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     net = Net(input_size, hidden_size, output_size).to(device)
 
     # Training
     # Loss Function
     criterion = nn.CrossEntropyLoss()
-    #optimizer = optim.SGD(net.parameters(), lr=lr)
+    # optimizer = optim.SGD(net.parameters(), lr=lr)
     optimizer = optim.Adam(net.parameters())
     uncert_train = []
     uncert_test = []
 
     for epoch in range(n_epochs):
         total_loss = 0
-        net.train()    
+        net.train()
         ### in training loop ###
-        for i in range(int(len(X_train)/batch_size)):
-            X_batch = X_train[i*batch_size:(i+1)*batch_size].to(device)
-            y_batch = y_train[i*batch_size:(i+1)*batch_size].to(device)
+        for i in range(int(len(X_train) / batch_size)):
+            X_batch = X_train[i * batch_size:(i + 1) * batch_size].to(device)
+            y_batch = y_train[i * batch_size:(i + 1) * batch_size].to(device)
             optimizer.zero_grad()
             pred = net(X_batch)
             loss = criterion(pred, y_batch)
             total_loss += loss
             loss.backward()
             optimizer.step()
-            
+
         # Testing    
-        if ((epoch+1) % report_every) == 0:
+        if ((epoch + 1) % report_every) == 0:
             net.eval()
             with torch.no_grad():
                 # Calculate train accuracy
                 target = y_train.to(device)
                 pred = net(X_train.to(device))
                 pred_label_train = torch.argmax(pred, dim=1)
-                train_acc = torch.sum(pred_label_train==target) / len(y_train)
-                # For task 2
-                for i in range(len(y_train)):
-                    if pred_label_train[i] == target[i]:
-                        uncert_train.append(0)
-                    else:
-                        uncert_train.append(1) 
+                train_acc = torch.sum(pred_label_train == target) / len(y_train)
 
                 # Calculate test accuracy
                 target = y_test.to(device)
                 pred = net(X_test.to(device))
                 pred_label_test = torch.argmax(pred, dim=1)
-                test_acc = torch.sum(pred_label_test==target) / len(y_test)
-                # For task 2
-                for i in range(len(y_test)):
-                    if pred_label_test[i] == target[i]:
-                        uncert_test.append(0)
-                    else:
-                        uncert_test.append(1) 
+                test_acc = torch.sum(pred_label_test == target) / len(y_test)
 
-        print('epoch: %d, loss: %.4f, train acc: %.2f, test acc: %.2f' % (epoch, total_loss/int(len(X_train)/batch_size), train_acc, test_acc))
-    return(uncert_train, uncert_test)
+            print('epoch: %d, loss: %.4f, train acc: %.2f, test acc: %.2f' % (epoch, total_loss / int(len(X_train) / batch_size), train_acc, test_acc))
 
-uncert_train, uncert_test = pred_coord(X_train, X_test, y_train, y_test)
+    return net
 
-# Task 2: Predict Uncertainity of the y-hat prediction
-# Dataset
-y_train_2 = torch.LongTensor(uncert_train).view(-1)
-y_test_2 = torch.LongTensor(uncert_test).view(-1)
 
-# Hyperparameter for task 2
-input_size = X_train.shape[1]
-hidden_size = 32
-output_size = 15 + 2 # p + No instruction given + Language
-lr = 0.01
-batch_size = 512
-n_epochs = 1000
-report_every = 100
+def pred_uncert(X, y):
+    # Data split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3, random_state=42)
 
-# model for task 2
-device_2 = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-net_2 = Net(input_size, hidden_size, output_size).to(device_2)
+    # Hyperparameter for task 2
+    input_size = X_train.shape[1]
+    hidden_size = 32
+    output_size = 2
+    lr = 0.01
+    batch_size = 512
+    n_epochs = 30
+    report_every = 3
 
-def pred_uncert(X_train, X_test, y_train, y_test):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     net = Net(input_size, hidden_size, output_size).to(device)
 
     # Training
     # Loss Function
     criterion = nn.CrossEntropyLoss()
-    #optimizer = optim.SGD(net.parameters(), lr=lr)
     optimizer = optim.Adam(net.parameters())
 
     for epoch in range(n_epochs):
         total_loss_2 = 0
-        net.train()    
+        net.train()
         ### in training loop ###
-        for i in range(int(len(X_train)/batch_size)):
-            X_batch = X_train[i*batch_size:(i+1)*batch_size].to(device)
-            y_batch = y_train[i*batch_size:(i+1)*batch_size].to(device)
+        for i in range(int(len(X_train) / batch_size)):
+            X_batch = X_train[i * batch_size:(i + 1) * batch_size].to(device)
+            y_batch = y_train[i * batch_size:(i + 1) * batch_size].to(device)
             optimizer.zero_grad()
             pred = net(X_batch)
             loss = criterion(pred, y_batch)
             total_loss_2 += loss
             loss.backward()
             optimizer.step()
-            
+
         # Testing    
-        if ((epoch+1) % report_every) == 0:
+        if ((epoch + 1) % report_every) == 0:
             net.eval()
             with torch.no_grad():
                 # Calculate train accuracy
                 target = y_train.to(device)
                 pred = net(X_train.to(device))
                 pred_label_train = torch.argmax(pred, dim=1)
-                train_acc_2 = torch.sum(pred_label_train==target) / len(y_train)
+                train_acc_2 = torch.sum(pred_label_train == target) / len(y_train)
 
                 # Calculate test accuracy
                 target = y_test.to(device)
                 pred = net(X_test.to(device))
                 pred_label_test = torch.argmax(pred, dim=1)
-                test_acc_2 = torch.sum(pred_label_test==target) / len(y_test)
+                test_acc_2 = torch.sum(pred_label_test == target) / len(y_test)
 
-        #print('epoch: %d, loss: %.4f, train acc: %.2f, test acc: %.2f' % (epoch, total_loss_2/int(len(X_train)/batch_size), train_acc_2, test_acc_2))
-        print('epoch: %d, loss: %.4f, train acc: %.2f, test acc: %.2f' % (epoch, total_loss_2/int(len(X_train)/batch_size), train_acc_2, test_acc_2))
+            # print('epoch: %d, loss: %.4f, train acc: %.2f, test acc: %.2f' % (epoch, total_loss_2/int(len(X_train)/batch_size), train_acc_2, test_acc_2))
+            print('epoch: %d, loss: %.4f, train acc: %.2f, test acc: %.2f' % (epoch, total_loss_2 / int(len(X_train) / batch_size), train_acc_2, test_acc_2))
 
-pred_uncert(X_train, X_test, y_train_2, y_test_2)
-
-
+    return net
 
 
+if __name__ == '__main__':
+    # Load the dataset
+    X_df = pd.read_json('data/X_DM.json', orient='index')
+    y_df = pd.read_json('data/y_DM.json', orient='index')
+    tensor_X, tensor_y = convert_datatype(X_df, y_df)
+    tensor_X, tensor_y = tensor_X[:], tensor_y[:]
+    print(tensor_X.shape, tensor_y.shape)
+
+    half = int(len(tensor_X)/2)
+    X_1 = tensor_X[:half]
+    y_1 = tensor_y[:half]
+    X_2 = tensor_X[half:]
+    y_2 = tensor_y[half:]
+
+    print("Training model 1")
+    net_task_1 = pred_coord(X_1, y_1)
+    net_task_1.eval()
+
+    print("Generating y for model 2 using model 1")
+    y_uncert = net_task_1(X_2.to(device))
+    y_uncert_class = torch.argmax(y_uncert, dim=1)
+    bool_tensor = (y_uncert_class != y_2)
+    long_tensor = bool_tensor.to(torch.long)
+
+    # Task 2: Predict Uncertainity of the y-hat prediction
+    print("Training model 2")
+    net_task_2 = pred_uncert(X_2, long_tensor)
+    net_task_2.eval()
 
 """
 # Training
