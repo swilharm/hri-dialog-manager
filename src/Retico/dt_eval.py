f4075d9931e8eb3742955468b8066df6d446b0ee
Sebastian Wilharm <swilharm@uni-potsdam.de>
2022-09-13 20:47:51 +0200

    Fixed dataset loading in decision tree Fixed a couple bugs

--- src/Retico/dt_eval.py
+++ src/Retico/dt_eval.py
@@ -1,22 +1,20 @@
-from ast import main
 import json
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from tqdm import tqdm 
+from tqdm import tqdm
 
 
 def load_data(x_path, y_path, no_puzzlepieces):
     with open(x_path) as f:
         data = json.load(f)
 
-    X = pd.DataFrame.from_dict(data,orient='index')
+    X = pd.DataFrame.from_dict(data, orient='index')
 
     with open(y_path) as f:
         data = json.load(f)
 
-    y = pd.DataFrame.from_dict(data,orient='index')
-    
+    y = pd.DataFrame(pd.DataFrame.from_dict(data).values.T)
+
     df_new = X.drop(['Instruction', 'Coordinate'], axis=1)
 
     instr_df = pd.DataFrame(df_new['Instruction Confidence'].to_list())
@@ -41,51 +39,47 @@ def load_data(x_path, y_path, no_puzzlepieces):
 
     return final_df_noisy, y
 
+
 def dt(instr_confs, lv_coord_confs, g_coord_confs):
-    instr_thresh = 0.90
-    coord_thresh = 0.80
+    instr_thresh = 0.8
+    coord_thresh = 0.1
     if instr_confs[2] > instr_thresh:
-            return 1
+        return 1
     else:
         if instr_confs[0] > instr_thresh > instr_confs[1]:
             coordinate = np.argmax(lv_coord_confs)
             probability = lv_coord_confs[coordinate]
             if probability > coord_thresh:
-                return coordinate
+                return coordinate + 2
             else:
                 return 0
         elif instr_confs[0] < instr_thresh < instr_confs[1]:
             coordinate = np.argmax(g_coord_confs)
             probability = g_coord_confs[coordinate]
             if probability > coord_thresh:
-                return coordinate
+                return coordinate + 2
             else:
                 return 0
-        elif instr_confs[0] > instr_thresh and instr_confs[1]:
-            mean = [(lv+g)/2 for lv, g in zip(lv_coord_confs, g_coord_confs)]
+        elif instr_confs[0] > instr_thresh and instr_confs[1] > instr_thresh:
+            mean = [(lv + g) / 2 for lv, g in zip(lv_coord_confs, g_coord_confs)]
             max_mean = np.argmax(mean)
             if mean[max_mean] > coord_thresh:
-                return max_mean
+                return max_mean + 2
             else:
                 return 0
         elif instr_confs[0] < instr_thresh and instr_confs[1] < instr_thresh:
             return 0
     return None
 
+
 if __name__ == '__main__':
     NUM_PIECES = 15
-    X, y = load_data('src/Retico/data/X_DM.json', 'src/Retico/data/y_DM.json', NUM_PIECES)
+    X, y = load_data('data/X_DM.json', 'data/y_DM.json', NUM_PIECES)
+    X, y = X[:], y[:]
     preds = []
-    for _, row in tqdm(X.iterrows(), total=len(X)):     
-        pred = dt(row[:3].values, row[3:3+NUM_PIECES], row[3+NUM_PIECES:])
+    for row in tqdm(X.values, total=len(X)):
+        pred = dt(row[:3], row[3:3 + NUM_PIECES], row[3 + NUM_PIECES:])
         preds.append(pred)
-    
-    acc = sum([i==j for i, j in zip(preds, y.values)])/len(y)
+
+    acc = sum([i == j for i, j in zip(preds, y.values)]) / len(y)
     print(acc)
-        
-    
-
-
-
-
-
